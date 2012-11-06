fba2bb6b993e47b94b8727214068906b7bfe6356
Yu-Ching Wang <destinyisathand@gmail.com>
2012-11-06 08:27:33 -0800

    Some work on styling, need to take input and adjust.

--- public_html/results.php
+++ public_html/results.php
@@ -1,22 +1,25 @@
 <html>
-	<body onload="initialize()" onunload="GUnload()">
-		<form>
-			Location: <input type="text" name="location" value="<?php echo $_GET["location"] ?>"><br>
-			Keyword: <input type="text" name="keyword" value="<?php echo $_GET["keyword"] ?>"><br>
-			Salary: $<input type="text" name="salary" value="<?php echo $_GET["salary"] ?>"><br>
-			Education: <input type="text" name="education" value="<?php echo $_GET["education"] ?>"><br>
-			<input type="submit" value="Redo"><br>
-		</form>
-		<script type="text/javascript">
-			function initialize() {
-				if (GBrowserIsCompatible()) {
-					var map = new GMap2(document.getElementById("map_canvas");
-					map.setCenter(new GLatLng(0, 0),13);
-					map.setUIToDefault();
-				}
-			}
-		</script>
-		<div id="map_canvas" style="width: 500px; height: 300px"></div>
-	</body>
+        <body onload="initialize()" onunload="GUnload()">
+                <form>
+                        <div class="label">Location</div>
+                        <input type="text" name="location" value="<?php echo $_GET["location"] ?>"><br>
+                        <div class="label">Keyword</div>
+                        <input type="text" name="keyword" value="<?php echo $_GET["keyword"] ?>"><br>
+                        <div class="label">Salary</div>
+                        <input type="text" name="salary" value="<?php echo $_GET["salary"] ?>"><br>
+                        <div class="label">Education</div>
+                        <input type="text" name="education" value="<?php echo $_GET["education"] ?>"><br>
+                        <input type="submit" value="Redo"><br>
+                </form>
+                <script type="text/javascript">
+                        function initialize() {
+                                if (GBrowserIsCompatible()) {
+                                        var map = new GMap2(document.getElementById("map_canvas");
+                                        map.setCenter(new GLatLng(0, 0),13);
+                                        map.setUIToDefault();
+                                }
+                        }
+                </script>
+                <div id="map_canvas" style="width: 500px; height: 300px"></div>
+        </body>
 </html>
-
