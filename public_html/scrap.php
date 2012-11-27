e017667cb6885608127f53e54d4b06b904141495
Hon-ming Chen <hmchen@gmail.com>
2012-11-26 16:34:36 -0800

    First pass at scraping dice.com
    Still need to follow the link for each job post and scrap it for salary
    and requirements

--- public_html/scrap.php
+++ public_html/scrap.php
@@ -1,6 +1,6 @@
 <?php
-function getArticles($page) {
-	global $articles;
+function getMonsterJobPostings($page) {	
+	global $jobPostings;
 	$html = new simple_html_dom();
 	$html->load_file($page);
 	
@@ -11,9 +11,8 @@ function getArticles($page) {
 		$company = $element->children(1)->children(0)->children(1)->children(0)->children(0)->innertext;
 		$salary = $element->children(1)->children(0)->children(1)->children(0)->children(2)->innertext;		
 		$link = $element->children(1)->children(0)->children(0)->first_child()->href;
-
-		
-		$articles[] = array(
+	
+		$jobPostings[] = array(
 			'title' => $title,
 			'city' => $city,
 			'company' => $company,
@@ -24,29 +23,114 @@ function getArticles($page) {
 	}
 }
 
+function getDiceJobPostings($page) {	
+	global $jobPostings;
+	$html = file_get_html($page);
+	
+	$items = $html->find('tr.gold, tr.STDsrRes');
+	foreach ($items as $element) {
+		$title = $element->children(0)->children(0)->first_child()->innertext;
+		$city = $element->children(2)->innertext;
+		$company = $element->children(1)->children(0)->innertext;		
+		$link = "http://seeker.dice.com/" . $element->children(0)->children(0)->first_child()->href;
+	
+		$jobPostings[] = array(
+			'title' => $title,
+			'city' => $city,
+			'company' => $company,
+			'salary' => $salary,
+			'requirements' => "TODO",
+			'link' => $link
+		);
+	}
+}
+
+// TODO
+function getRequirements($page) {
+	$jobRequirements = array();
+	$html = new simple_html_dom();
+	$html->load_file($page);
+}
+
+// Ouput array of dictionary items to HTML
+function outputDicDataToHTML($items) {
+	echo "<div>";
+	foreach ($items as $item) {
+		foreach ($item as $key => $value) {
+			echo $key . ": " . $value . "<br>";
+		}
+		echo "<br>";
+	}
+	echo "</div>";
+}
+
 function writeToJsonFile($inputData) {
 	$fp = fopen('results.json', 'w');
     fwrite($fp, json_encode($inputData));
     fclose($fp);
 }
 
-include('simple_html_dom.php');
-$articles = array();
-// getArticles('monster.htm');
-getArticles("http://jobsearch.monster.com/search/?where=san-jose__2C-ca");
-
-foreach ($articles as $item) {
-	echo "<div>";
-    echo 'title: ' . $item['title'] . "<br>";
-	echo 'company: ' . $item['company'] . "<br>";
-	echo 'city: ' . $item['city'] . "<br>";
-	echo 'salary: ' . $item['salary'] . "<br>";
-	echo 'link: ' . $item['link'] . "<br>";
- 	echo "<br>";
-    echo "</div>";
+function createMonsterURL() {
+	global $inLocation, $inKeyword;
+	$data = array();
+	$url = "http://jobsearch.monster.com/search/?";
+	
+	if (strlen($inKeyword) > 0) {
+		$data['q'] = $inKeyword;
+	}
+	
+	if (strlen($inLocation) > 0) {
+		$data["where"] = $inLocation;
+	}
+	
+	$url .= convertToMonsterEncoding(http_build_query($data));
+	
+	return $url;
 }
 
-// writeToJsonFile($articles);
+function createDiceURL() {
+	global $inLocation, $inKeyword;
+	$data = array(
+		'FREE_TEXT' => $inKeyword,
+		'WHERE' => $inLocation
+	);
+	$url = "http://seeker.dice.com/jobsearch/servlet/JobSearch?op=300&N=0&Hf=0&NUM_PER_PAGE=30&Ntk=JobSearchRanking&Ntx=mode+matchall&AREA_CODES=&AC_COUNTRY=1525&QUICK=1&ZIPCODE=&RADIUS=64.37376&ZC_COUNTRY=0&COUNTRY=1525&STAT_PROV=0&METRO_AREA=33.78715899%2C-84.39164034&TRAVEL=0&TAXTERM=0&SORTSPEC=0&FRMT=0&DAYSBACK=30&LOCATION_OPTION=2&";
+	
+	$url .= http_build_query($data);
+	
+	return $url;
+}
+
+// TODO: finish converting special characters to be compatible with monster.com
+function convertToMonsterEncoding($url) {
+	$url = str_replace("+","-",$url);
+	return $url;
+}
+
+// ******************Start Scraping******************
+// input variables
+$inLocation = $_POST['location'];
+$inKeyword = $_POST['keyword'];
+$inSalary = $_POST['salary'];
+$inEducation = $_POST['education'];
+
+// echo createMonsterURL() . "<br><br>";
+// echo createDiceURL() . "<br><br>";
+// echo $inLocation . $inKeyword . $inSalary . $inEducation . "<br><br>";
+
+include('simple_html_dom.php');
+$jobPostings = array();
+// getJobPostings(createMonsterURL());
+// getJobPostings('monster.htm');
+// getJobPostings("http://jobsearch.monster.com/search/?where=san-jose__2C-ca");
+
+getDiceJobPostings('dice.htm');
+
+
+outputDicDataToHTML($jobPostings);
+
+
+// writeToJsonFile($jobPostings);
 
 
 
