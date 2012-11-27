afa80c46b1890329714ad2f1f5ab343e4a44053c
Hon-ming Chen <hmchen@gmail.com>
2012-11-27 10:02:27 -0800

    modified index.html to work with scrap.php and redirect output to results.html, added simple_html_dom.php library, and sample json output file

--- public_html/scrap.php
+++ public_html/scrap.php
@@ -1,9 +1,8 @@
+
 <?php
 function getMonsterJobPostings($page) {	
-	global $jobPostings;
-	$html = new simple_html_dom();
-	$html->load_file($page);
-	
+	global $monsterJobPostings;
+	$html = file_get_html($page);
 	$items = $html->find('tr.even, tr.odd'); 
 	foreach ($items as $element) {
 		$title = $element->children(1)->children(0)->children(0)->first_child()->innertext;
@@ -12,7 +11,7 @@ function getMonsterJobPostings($page) {
 		$salary = $element->children(1)->children(0)->children(1)->children(0)->children(2)->innertext;		
 		$link = $element->children(1)->children(0)->children(0)->first_child()->href;
 	
-		$jobPostings[] = array(
+		$monsterJobPostings[] = array(
 			'title' => $title,
 			'city' => $city,
 			'company' => $company,
@@ -21,10 +20,13 @@ function getMonsterJobPostings($page) {
 			'link' => $link
 		);
 	}
+	// clear and unset to prevent memory leaks
+	$html->clear();
+	unset($html);
 }
 
 function getDiceJobPostings($page) {	
-	global $jobPostings;
+	global $diceJobPostings;
 	$html = file_get_html($page);
 	
 	$items = $html->find('tr.gold, tr.STDsrRes');
@@ -34,22 +36,28 @@ function getDiceJobPostings($page) {
 		$company = $element->children(1)->children(0)->innertext;		
 		$link = "http://seeker.dice.com/" . $element->children(0)->children(0)->first_child()->href;
 	
-		$jobPostings[] = array(
+		$diceJobPostings[] = array(
 			'title' => $title,
 			'city' => $city,
 			'company' => $company,
-			'salary' => $salary,
+			'salary' => "TODO",
 			'requirements' => "TODO",
 			'link' => $link
 		);
 	}
+	// clear and unset to prevent memory leaks
+	$html->clear();
+	unset($html);
 }
 
 // TODO
-function getRequirements($page) {
-	$jobRequirements = array();
-	$html = new simple_html_dom();
-	$html->load_file($page);
+function getMonsterRequirements($page) {
+
+}
+
+// TODO 
+function getDiceRequirements($page) {
+	
 }
 
 // Ouput array of dictionary items to HTML
@@ -65,7 +73,7 @@ function outputDicDataToHTML($items) {
 }
 
 function writeToJsonFile($inputData) {
-	$fp = fopen('results.json', 'w');
+	$fp = fopen('./results/results.json', 'w');
     fwrite($fp, json_encode($inputData));
     fclose($fp);
 }
@@ -84,7 +92,7 @@ function createMonsterURL() {
 	}
 	
 	$url .= convertToMonsterEncoding(http_build_query($data));
-	
+	// echo $url . "<br><br>";
 	return $url;
 }
 
@@ -97,7 +105,7 @@ function createDiceURL() {
 	$url = "http://seeker.dice.com/jobsearch/servlet/JobSearch?op=300&N=0&Hf=0&NUM_PER_PAGE=30&Ntk=JobSearchRanking&Ntx=mode+matchall&AREA_CODES=&AC_COUNTRY=1525&QUICK=1&ZIPCODE=&RADIUS=64.37376&ZC_COUNTRY=0&COUNTRY=1525&STAT_PROV=0&METRO_AREA=33.78715899%2C-84.39164034&TRAVEL=0&TAXTERM=0&SORTSPEC=0&FRMT=0&DAYSBACK=30&LOCATION_OPTION=2&";
 	
 	$url .= http_build_query($data);
-	
+	// echo $url . "<br><br>";
 	return $url;
 }
 
@@ -107,33 +115,51 @@ function convertToMonsterEncoding($url) {
 	return $url;
 }
 
+// TODO: write a better merge function, it should check for potential duplicates
+function mergeJobPostings($monsterJP, $diceJP) {
+	return array_merge($monsterJP, $diceJP);
+}
+
+// echo "Hello" . "<br><br>";
 // ******************Start Scraping******************
-// input variables
-$inLocation = $_POST['location'];
-$inKeyword = $_POST['keyword'];
-$inSalary = $_POST['salary'];
-$inEducation = $_POST['education'];
-
-// echo createMonsterURL() . "<br><br>";
-// echo createDiceURL() . "<br><br>";
-// echo $inLocation . $inKeyword . $inSalary . $inEducation . "<br><br>";
-
+// import library(s)
 include('simple_html_dom.php');
+
+// input variables
+$inLocation = $_GET['location'];
+$inKeyword = $_GET['keyword'];
+$inSalary = $_GET['salary'];
+$inEducation = $_GET['education'];
+
+// array of dictionary items holding job postings
+$monsterJobPostings = array();
+$diceJobPostings = array();
 $jobPostings = array();
-// getJobPostings(createMonsterURL());
-// getJobPostings('monster.htm');
-// getJobPostings("http://jobsearch.monster.com/search/?where=san-jose__2C-ca");
+echo "before scrapping" . "<br><br>";
+// scrap websites
+// getMonsterJobPostings('monster.htm');
+// getDiceJobPostings('dice.htm');
+getDiceJobPostings(createDiceURL());
+getMonsterJobPostings(createMonsterURL());
+echo "after scrapping" . "<br><br>";
+// merge job postings
+$jobPostings = mergeJobPostings($monsterJobPostings, $diceJobPostings);
 
-getDiceJobPostings('dice.htm');
-
-
+// output to html
 outputDicDataToHTML($jobPostings);
 
+// write to json file
+writeToJsonFile($jobPostings);
 
-// writeToJsonFile($jobPostings);
-
-
-
-$html->clear();
-unset($html);
+// redirect after script completion
+echo "<script>window.location = './results.html'</script>";
 ?>
+
+
+
+
+
+
+
+
+
