a329d3fa597d14cf63fde1faf569ce5ec82cfb0b
Colin B <lanzaa@gmail.com>
2012-11-30 21:01:44 -0800

    Cleaning the results.php html. Fixing bugs.
    I fixed a lot of odd things... The javascript appending still sucks, but
    it is a little better and cleaner. It actually puts things in divs... I
    fixed the display of the date with javascript. Not sure why that was
    broken.
    
    This project still needs lots of cleanup, but I don't think anyone cares
    enough to do it.

--- public_html/results.php
+++ public_html/results.php
@@ -1,68 +1,64 @@
 <!DOCTYPE html>
 <html>
-<!--Load the Google Maps API with the places library.-->
-<script src="https://maps.googleapis.com/maps/api/js?&sensor=true&libraries=places"></script>
-<!-- Load jquery to read the JSON from a file. -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<!-- Load the map interface stuff. -->
-<script src="map.js"></script>
-
 <head>
-	<link rel = "stylesheet" type = "text/css" href = "style.css"/>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <!--Load the Google Maps API with the places library.-->
+    <script src="https://maps.googleapis.com/maps/api/js?&sensor=true&libraries=places"></script>
+    <!-- Load jquery to read the JSON from a file. -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <!-- Load the map interface stuff. -->
+    <script src="map.js"></script>
+
+    <link rel = "stylesheet" type = "text/css" href = "style.css"/>
 </head>
 <body id="div-my-tabl">
-
-<tr><td></td></tr>
-</table>
         <div class = "subHeadline"> Didn't find what you were looking for? </div>
         <div class = "search">
 	
-         <form name="user_input" action="scrap.php" method="get">
-            <p>
-             <div class="label"> Location:
-                  <input type="text" name="location" value="<?php echo $_GET['location']?>">
-               <div class="label">Keyword:
-                  <input type="text" name="keyword" value="<?php echo $_GET['keyword']?>">
-               </div>
-               <div class="label">Salary:
-			<select name="salary">
-                        <option value=""></option>
-                        <option value="30000">$30,000.00</option>
-                        <option value="40000">$40,000.00</option>
-                        <option value="50000">$50,000.00</option>
-                        <option value="60000">$60,000.00</option>
-			<option value="70000">$70,000.00</option>
-			<option value="80000">$80,000.00</option>
-			<option value="90000">$90,000.00</option>
-			<option value="100000">$100,000.00</option>
-			<option value="125000">$125,000.00</option>
-			<option value="150000">$150,000.00</option>
-                        </select>                  
-               </div>
-               <div class="label">Education:
-                  <select name="education">
-			<option value=""></option>
-			<option value="High-School">High School</option>
-			<option value="Associate-Degree">Associate Degree</option>
-			<option value="Bachelor's-Degree">Bachelor's Degree</option>
-			<option value="Advanced-Degree">Advanced Degree</option>
-		</select>
-               </div>
-            </p>
-          <input type="submit" value="Search">
-         </form>
-	
-     </div>
-      <div class="results">
-         <div class="title"></div>
-      </div>
+		<form name="user_input" action="scrap.php" method="get">
+			<p>
+			<div class="label"> Location:
+				<input type="text" name="location" value="<?php echo $_GET['location']?>">
+			</div>
+			<div class="label">Keyword:
+				<input type="text" name="keyword" value="<?php echo $_GET['keyword']?>">
+			</div>
+			<div class="label">Salary:
+				<select name="salary">
+				<option value=""></option>
+				<option value="30000">$30,000.00</option>
+				<option value="40000">$40,000.00</option>
+				<option value="50000">$50,000.00</option>
+				<option value="60000">$60,000.00</option>
+				<option value="70000">$70,000.00</option>
+				<option value="80000">$80,000.00</option>
+				<option value="90000">$90,000.00</option>
+				<option value="100000">$100,000.00</option>
+				<option value="125000">$125,000.00</option>
+				<option value="150000">$150,000.00</option>
+				</select>
+			</div>
+			<div class="label">Education:
+				<select name="education">
+					<option value=""></option>
+					<option value="High-School">High School</option>
+					<option value="Associate-Degree">Associate Degree</option>
+					<option value="Bachelor's-Degree">Bachelor's Degree</option>
+					<option value="Advanced-Degree">Advanced Degree</option>
+				</select>
+			</div>
+			</p>
+			<input type="submit" value="Search">
+		</form>
+</div>
+<div class="results">
+	<div class="title"></div>
+</div>
  
 <h2 id="DATE">	
 <script language="javascript">
 <!--
 today = new Date();
-document.write("<BR> Searching jobs on the date of: ", today.getMonth()+1,"/",today.getDate(),"/",today.getYear());
+$("#DATE").text("Searching jobs on the date of: "+(today.getMonth()+1)+"/"+today.getDate()+"/"+today.getFullYear());
 //-->
 </script> </h2>
 
@@ -80,42 +76,18 @@ document.write("<BR> Searching jobs on the date of: ", today.getMonth()+1,"/",to
 	<script>
 	$("document").ready(function() {
 	    $.getJSON("./results/results.json", function(data) {
-	        $("#div-my-table").text("<table>");
 	        $.each(data, function(i, item) {
-			
-				$("#div-my-tabl").append("<tr>");
-				$("#div-my-tabl").append("<td>");
-				
-				$("#div-my-tabl").append("<div>");
-				$("#div-my-tabl").append('<a href="' + $(item).attr("link") + '">' + $(item).attr("title") + '</a>');
-				$("#div-my-tabl").append("</div>");
-				
-				$("#div-my-tabl").append("<div>");
-				$("#div-my-tabl").append("</div>");
-				$("#div-my-tabl").append(item.company);
-				$("#div-my-tabl").append("<div>");
-				$("#div-my-tabl").append("</div>");	
-			        
-
-				$("#div-my-tabl").append(item.city);
-                                $("#div-my-tabl").append("<div>");
-                                $("#div-my-tabl").append("</div>");
-
-				$("#div-my-tabl").append(item.salary);
-                                $("#div-my-tabl").append("<div>");
-                                $("#div-my-tabl").append("</div>");
-				
-				$("#div-my-tabl").append(item.requirements);
-				$("#div-my-tabl").append("</td>");
-	            		$("#div-my-tabl").append("</tr>");
-				$("#div-my-tabl").append("<br />");
-				$("#div-my-tabl").append("<br />");
+				var toappend = "<div>";
+				toappend += '<a href="' + item.link + '">' + item.title + '</a>';
+				toappend += "</div>";
+				toappend += '<div class="company">'+(item.company||'')+'</div>';
+				toappend += '<div class="city">'+(item.city||'')+'</div>';
+				toappend += '<div class="salary">'+(item.salary||'')+'</div>';
+				toappend += '<div class="requirements">'+(item.requirements||'')+'</div>';
+				$("#div-my-tabl").append(toappend+"\n");
+				$("#div-my-tabl").append("<br />\n");
 				// $("#div-my-tabl").append("<br />");
-			
 	        });
-		
-	        $("#div-my-table").append("</table>");
-
 	    });
 	});
 	</script>
