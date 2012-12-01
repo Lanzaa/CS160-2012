db9171944c5cf55cee916be65408dd0e7a52e950
Colin B <lanzaa@gmail.com>
2012-11-30 19:38:51 -0800

    Add files from live site. Prepare for merge.
    Hopefully this is all the needed files.

---
+++ public_html/map.js
@@ -0,0 +1,124 @@
+// when the page is loaded, call init function.
+google.maps.event.addDomListener(window, 'load', init);
+
+// Variables for use internally.
+var map, markers = [];
+
+// Initialize the map, assign autocomplete to location text field.
+function init() {
+	// required options for the map.
+	mapOptions = { zoom:10, center:new google.maps.LatLng(0, 0), mapTypeId:google.maps.MapTypeId.ROADMAP };
+	// the field object where location is typed in.
+	input = document.getElementsByName('location')[0]; // TODO hard coded field.
+	//enable autocomplete on input field, and update map when autocomplete option clicked.
+	autocomplete = new google.maps.places.Autocomplete(input);
+	google.maps.event.addListener(autocomplete, 'place_changed', function() {
+		map.setCenter(autocomplete.getPlace().geometry.location);
+	});
+	// create a map, listener for map location changed
+	map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+	google.maps.event.addListener(map, 'dragend', function() { updateLocation(input); });
+	// initially place the map where the user has given us a location.
+	updateMap(input.value);
+	// parse the json file.  TODO hard coded directory, keep up to date!
+	parseJSON("./results/results.json");
+}
+
+// Update the map's displayed area based on location supplied by user.
+function updateMap(location) {
+	// if the location is not empty, update the map.
+	if (location != "") {
+		new google.maps.Geocoder().geocode({ 'address': location }, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK)
+				map.setCenter(results[0].geometry.location);
+		});
+	}
+}
+
+// Update the location text field based on where the map has been moved to.
+function updateLocation(field) {
+	cur = field.value;
+	new google.maps.Geocoder().geocode({ 'latLng': map.getCenter() }, function(r, s) {
+		if (s == google.maps.GeocoderStatus.OK) {
+			for (i = 0; i < r.length; i++) {
+				for (j = 0; j < r[i].types.length; j++) {
+					if (r[i].types[j] == "locality") {
+						console.log(r[i].formatted_address);
+						field.value = r[i].formatted_address;
+					}
+				}
+			}
+		}
+		// Perform a new search
+		if (cur != field.value)
+			document.forms["user_input"].submit();
+	});
+}
+
+// Add a marker to the map given a business name and city.
+function addMarker(json) {
+	if (json.name == "" || json.city == "")
+		return;
+	new google.maps.Geocoder().geocode({ 'address':json.city }, function(r, s) {
+		if (s == google.maps.GeocoderStatus.OK) {
+			new google.maps.places.PlacesService(map).
+				nearbySearch({ location:r[0].geometry.location, radius:50000, name:json.name }, createMarker(json));
+				} else { // sleep 1/4 of a second and try again.
+				console.log(s);
+			setTimeout(function() { addMarker(json); }, 300);
+		}
+	});
+}
+
+// Create a marker, place on the map, store in markers array.
+function createMarker(json) {
+	out = function(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			marker = new google.maps.Marker({ map:map, position:results[0].geometry.location });
+			markers.push(marker);
+			infowindow = new google.maps.InfoWindow({ disableAutoPan:true });
+			google.maps.event.addListener(marker, 'mouseover', function() {
+					infowindow.setContent(getContent(json));
+					infowindow.open(map, this);
+			});
+			google.maps.event.addListener(marker, 'mouseout', function() {
+				infowindow.close();
+			});
+			google.maps.event.addListener(marker, 'click', function() {
+				window.location = json.link;
+			});
+			resizeMap();
+		}
+	}
+	return out;
+}
+
+// Parse a set of JSON objects into markers.
+function parseJSON(path) {
+	$.getJSON(path, function(data) {
+		$.each(data, function(i, item) {
+			addMarker(item);
+		});
+	});
+}
+
+// create a content string for a given json entry.
+function getContent(r) {
+	return r.company + " in " + r.city + " are hiring a(n) " + r.title + " for " + r.salary + " annually";
+}
+
+// Resize the map to fit all the markers.
+function resizeMap() {
+	// north, east, south, west, current lattitude, current longitude
+	var n = -90, e = -180, s = 90, w = 180;
+	for (i = 0; i < markers.length; i++) {
+		curLat = markers[i].position.lat();
+		curLng = markers[i].position.lng();
+		n = n < curLat ? curLat : n;
+		e = e < curLng ? curLng : e;
+		s = s > curLat ? curLat : s;
+		w = w > curLng ? curLng : w;
+	}
+	// update the map's display based on north-east and south-west corners
+	map.fitBounds(new google.maps.LatLngBounds(new google.maps.LatLng(s, w), new google.maps.LatLng(n, e)));
+}
---
+++ public_html/scrap.php
@@ -0,0 +1,221 @@
+
+<?php
+$salaryArray = array(
+	"$50,000 - $60,000",
+	"$55,000 - $65,000",
+	"$60,000 - $70,000",
+	"$65,000 - $75,000",
+	"$75,000 - $85,000",
+	"$85,000 - $95,000",
+	"$95,000 - $105,000",
+	"$105,000 - $115,000",
+	"$115,000 - $125,000",
+	"$125,000 - $135,000",
+);
+
+$requirementsArray = array(
+	"0-1 years experience",
+	"1-2 years experience",
+	"2-3 years experience",
+	"3-4 years experience",
+	"4-5 years experience",
+	"5-6 years experience",
+	"6-7 years experience",
+	"7-8 years experience",
+	"8+ years experience",
+	"10+ years experience"
+);
+
+function getMonsterJobPostings($page) {	
+	global $monsterJobPostings, $requirementsArray, $salaryArray;
+	$html = file_get_html($page);
+	$items = $html->find('tr.even, tr.odd'); 
+	foreach ($items as $element) {
+		$title = $element->children(1)->children(0)->children(0)->first_child()->title;
+		$city = $element->children(2)->children(0)->children(0)->first_child()->innertext;
+		$company = $element->children(1)->children(0)->children(1)->children(0)->children(0)->innertext;
+		$salary = $element->children(1)->children(0)->children(1)->children(0)->children(2)->innertext;		
+		$link = $element->children(1)->children(0)->children(0)->first_child()->href;
+		$requirements = $requirementsArray[rand(0,sizeof($requirementsArray) - 1)];
+		
+		/*if (sizeof($salary) == 0) {
+			$salary = $salaryArray[rand(0,sizeof($salaryArray) - 1)];
+		}*/
+	
+		$monsterJobPostings[] = array(
+			'title' => $title,
+			'city' => $city,
+			'company' => $company,
+			'salary' => $salary,
+			'requirements' => $requirements,
+			'link' => $link
+		);
+	}
+	// clear and unset to prevent memory leaks
+	$html->clear();
+	unset($html);
+}
+
+function getDiceJobPostings($page) {	
+	global $diceJobPostings, $requirementsArray, $salaryArray;;
+	$html = file_get_html($page);
+	
+	$items = $html->find('tr.gold, tr.STDsrRes');
+	foreach ($items as $element) {
+		$title = $element->children(0)->children(0)->first_child()->innertext;
+		$city = $element->children(2)->innertext;
+		$company = $element->children(1)->children(0)->innertext;		
+		$link = "http://seeker.dice.com/" . $element->children(0)->children(0)->first_child()->href;
+		$requirements = $requirementsArray[rand(0,sizeof($requirementsArray) - 1)];
+		//$salary = $salaryArray[rand(0,sizeof($salaryArray) - 1)];
+
+		$diceJobPostings[] = array(
+			'title' => $title,
+			'city' => $city,
+			'company' => $company,
+			'salary' => $salary,
+			'requirements' => $requirements,
+			'link' => $link
+		);
+	}
+	// clear and unset to prevent memory leaks
+	$html->clear();
+	unset($html);
+}
+
+// TODO
+function getMonsterRequirements($page) {
+
+}
+
+// TODO 
+function getDiceRequirements($page) {
+	
+}
+
+// Ouput array of dictionary items to HTML
+function outputDicDataToHTML($items) {
+	echo "<div>";
+	foreach ($items as $item) {
+		foreach ($item as $key => $value) {
+			echo $key . ": " . $value . "<br>";
+		}
+		echo "<br>";
+	}
+	echo "</div>";
+}
+
+function writeToJsonFile($inputData) {
+	$fp = fopen('./results/results.json', 'w');
+    fwrite($fp, json_encode($inputData));
+    fclose($fp);
+}
+
+function createMonsterURL() {
+	global $inLocation, $inKeyword;
+	global $inSalary, $inEducation;
+	$data = array();
+	$url = "http://jobsearch.monster.com/search/?";
+	
+	if (strlen($inKeyword) > 0) {
+		$data['q'] = $inKeyword;
+	}
+	
+	if (strlen($inLocation) > 0) {
+		$data["where"] = $inLocation;
+	}
+
+	if(isset($inSalary) && $inSalary != ""){
+		$data["salmin"] = $inSalary;
+		$data["saltyp"] = "1";
+		$data["nosal"] = "false";
+	}
+
+	if(isset($inEducation) && $inEducation != ""){
+		$data["eid"] = $inEducation;
+	}
+	
+	$url .= convertToMonsterEncoding(http_build_query($data));
+	 echo $url . "<br><br>";
+	return $url;
+}
+
+function createDiceURL() {
+	global $inLocation, $inKeyword;
+	$data = array(
+		'FREE_TEXT' => $inKeyword,
+		'WHERE' => $inLocation
+	);
+	$url = "http://seeker.dice.com/jobsearch/servlet/JobSearch?op=300&N=0&Hf=0&NUM_PER_PAGE=30&Ntk=JobSearchRanking&Ntx=mode+matchall&AREA_CODES=&AC_COUNTRY=1525&QUICK=1&ZIPCODE=&RADIUS=64.37376&ZC_COUNTRY=0&COUNTRY=1525&STAT_PROV=0&METRO_AREA=33.78715899%2C-84.39164034&TRAVEL=0&TAXTERM=0&SORTSPEC=0&FRMT=0&DAYSBACK=30&LOCATION_OPTION=2&";
+	
+	$url .= http_build_query($data);
+	// echo $url . "<br><br>";
+	return $url;
+}
+
+// TODO: finish converting special characters to be compatible with monster.com
+function convertToMonsterEncoding($url) {
+	$url = str_replace("+","-",$url);
+	return $url;
+}
+
+// Remove potential duplicates and merge
+function mergeJobPostings($monsterJP, $diceJP) {
+	// remove duplicate job posts from $diceJP
+	for ($i = 0; $i < sizeof($monsterJP); $i++) {
+		for ($j = 0; $j < sizeof($diceJP); $j++) {
+			$val = compareJobPosts($monsterJP[$i], $diceJP[$j]);
+			if ($val == 0) {
+				// TODO: delete the post with less information
+				unset($diceJP[$j]);
+				break;
+			}
+		}
+	}
+	$mergedJP = array_merge($monsterJP, $diceJP);
+	shuffle($mergedJP);
+	return $mergedJP;
+}
+
+function compareJobPosts($jp1, $jp2) {
+	return ( abs(strcmp($jp1['title'], $jp2['title'])) +
+			 abs(strcmp($jp1['city'], $jp2['city'])) +
+			 abs(strcmp($jp1['company'], $jp2['company'])) );
+}
+
+// echo "Hello" . "<br><br>";
+// ******************Start Scraping******************
+// import library(s)
+include('simple_html_dom.php');
+
+// input variables
+$inLocation = $_GET['location'];
+$inKeyword = $_GET['keyword'];
+$inSalary = $_GET['salary'];
+$inEducation = $_GET['education'];
+
+// array of dictionary items holding job postings
+$monsterJobPostings = array();
+$diceJobPostings = array();
+$jobPostings = array();
+
+// scrap websites
+// getMonsterJobPostings('monster.htm');
+// getDiceJobPostings('dice.htm');
+if((!(isset($inSalary)) || $inSalary == "") && (!isset($inEducation) || $inEducation == ""))
+	getDiceJobPostings(createDiceURL());
+getMonsterJobPostings(createMonsterURL());
+
+// merge job postings
+$jobPostings = mergeJobPostings($monsterJobPostings, $diceJobPostings);
+
+// output to html
+outputDicDataToHTML($jobPostings);
+
+// write to json file
+writeToJsonFile($jobPostings);
+
+// redirect after script completion
+echo "<script>window.location = './results.php?location={$_GET['location']}&keyword={$_GET['keyword']}&salary={$_GET['salary']}&education={$_GET['education']}'</script>";
+?>
+
